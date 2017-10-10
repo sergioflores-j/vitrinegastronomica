b1579417bee476eeeac791b132495b2ea8e48b1a
Sergio Eduardo Flores Junior <sergio@adapcon.com.br>
2017-10-10 19:21:51 -0300

    Teste

---
+++ src/br/com/vitrinegastronomica/controllers/HelloWorldController.java
@@ -0,0 +1,27 @@
+package br.com.vitrinegastronomica.controllers;
+
+import javax.enterprise.inject.Model;
+
+@Model
+public class HelloWorldController {
+
+	private String name;
+	private String label;
+
+	public void action() {
+		System.out.println("button clicked!");
+		label = "Hello " + name;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/br/com/vitrinegastronomica/daos/ProductDao.java
@@ -0,0 +1,35 @@
+package br.com.vitrinegastronomica.daos;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.inject.Inject;
+
+import br.com.vitrinegastronomica.models.Product;
+
+public class ProductDao {
+
+	@Inject
+	private EntityManager manager;
+
+	public List<Product> all() {
+		return manager.createQuery("select p from Product p", Product.class).getResultList();
+	}
+
+	public void save(Product product) {
+		manager.persist(product);
+	}
+
+	public Product findById(Integer id) {
+		return manager.find(Product.class, id);
+	}
+
+	public void remove(Product product) {
+		manager.remove(product);
+	}
+
+	public void update(Product product) {
+		manager.merge(product);
+	}
+
+}
