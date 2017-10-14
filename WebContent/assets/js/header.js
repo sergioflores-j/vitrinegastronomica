222fa86fa189514470c51619a40f891538979eb5
Sergio Eduardo Flores Junior <sergio@adapcon.com.br>
2017-10-13 21:11:22 -0300

    update header

---
+++ WebContent/assets/js/header.js
@@ -0,0 +1,82 @@
+/*<![CDATA[*/
+$(document).ready(function () {
+	menuToggle();
+});
+
+function checkLogin(){
+	return false;
+}
+
+function menuToggle() {
+	let cross = $('.cross');
+	let hamburger = $('.hamburger');
+	let menu = $('.menu');
+	let logout = $('.btn-logout');
+	let login = $('.btn-login');
+	let register = $('.btn-register');
+	let isLogged = checkLogin();
+	console.log("READY");
+
+	cross.hide();
+	hamburger.hide();
+	
+	if(!isLogged) logout.hide();
+	else {
+		login.hide();
+		register.hide();
+	}
+	
+	$(window).resize(function(){ 
+		let windowWidth = $(window).width();
+		if(windowWidth <= 768 && cross.is(':hidden')){
+			
+			if(!isLogged){
+				login.hide();
+				register.hide();
+			} else logout.hide();
+			
+			menu.hide();
+			hamburger.show();
+			cross.hide();
+			
+		} else if(windowWidth >= 769) {
+			
+			if(!isLogged){
+				login.show();
+				register.show();
+			} else logout.show();
+			
+			menu.show();
+			hamburger.hide();
+			cross.hide();
+		}
+	})
+	
+    hamburger.click(function() {
+    	
+    	if(!isLogged){
+			login.show();
+			register.show();
+		} else logout.show();
+    	
+        menu.slideToggle('slow', function() {
+        	hamburger.hide();
+            cross.show();
+        });
+    });
+    
+    cross.click(function () {
+    	
+    	if(!isLogged){
+			login.hide();
+			register.hide();
+		} else logout.hide();
+    	
+        menu.slideToggle('slow', function () {
+            cross.hide();
+            hamburger.show();
+        })
+    });
+}
+
+/*]]>*/
